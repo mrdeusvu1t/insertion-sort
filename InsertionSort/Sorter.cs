3e31d88fcdad89018e6a68153e570da7c1b16176
Yury Hryshanau <mr.deusvult@gmail.com>
2020-10-26 06:19:24 +0300

    Update

--- InsertionSort/Sorter.cs
+++ InsertionSort/Sorter.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Globalization;
 
 // ReSharper disable InconsistentNaming
 #pragma warning disable SA1611
@@ -13,7 +14,23 @@ namespace InsertionSort
         public static void InsertionSort(this int[] array)
         {
             // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        var temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -22,7 +39,46 @@ namespace InsertionSort
         public static void RecursiveInsertionSort(this int[] array)
         {
             // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length <= 1)
+            {
+                return;
+            }
+
+            Sort(array, 0, 1);
+        }
+
+        public static void Sort(this int[] array, int i, int j)
+        {
+            if (j > 0)
+            {
+                Sort(array, i, j - 1);
+            }
+            else
+            {
+                i++;
+                j = i + 1;
+                if (j <= array.Length - 1)
+                {
+                    Sort(array, i, j);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (array[j - 1] > array[j])
+            {
+                var temp = array[j - 1];
+                array[j - 1] = array[j];
+                array[j] = temp;
+                Sort(array, i, j - 1);
+            }
         }
     }
 }
